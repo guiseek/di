694f49da67ad1e1f30e4f9fd68dd09d76bf986c6
Gui Seek <guiseek@gmail.com>
2023-06-09 13:54:32 -0300

    feat: dependency injection

---
+++ src/main.ts
@@ -0,0 +1,97 @@
+import {inject, register} from './di'
+import './style.css'
+
+/**
+ * Esta é uma dependência
+ * É ela quem nos fornece
+ * dados que precisaremos
+ */
+class Dependencia {
+  planetas = [
+    'Mercúrio',
+    'Vênus',
+    'Terra',
+    'Marte',
+    'Júpiter',
+    'Saturno',
+    'Urano',
+    'netuno',
+  ]
+}
+
+/**
+ * Este é um DataService
+ * E ele depende de quem
+ * irá fornecer os dados
+ */
+class DataService {
+  constructor(readonly dependencia: Dependencia) {}
+}
+
+/**
+ * Esta é uma abstração
+ * Este não tem retorno
+ * Só define o contrato
+ */
+abstract class Planetas {
+  abstract pegaPlanetas(): string[]
+}
+
+/**
+ * A concreta implementa
+ * Ela cumpre o contrato
+ * Ela depende dos dados
+ */
+class PlanetasImpl implements Planetas {
+  constructor(private servico: DataService) {}
+
+  pegaPlanetas() {
+    return this.servico.dependencia.planetas
+  }
+}
+
+/**
+ * Este é um registrador
+ * Organiza dependências
+ *
+ * Responda as perguntas
+ * 1. Quem vamos chamar?
+ * 2. Quem devemos usar?
+ * 3. E de quem precisa?
+ */
+register(
+  {
+    for: Dependencia,
+  },
+  {
+    for: DataService,
+    add: [Dependencia],
+  },
+  {
+    for: Planetas,
+    use: PlanetasImpl,
+    add: [DataService],
+  }
+)
+
+class AppRoot extends HTMLElement {
+  planetas = inject(Planetas)
+
+  connectedCallback() {
+    const select = document.createElement('select')
+
+    this.planetas.pegaPlanetas().forEach((planeta) => {
+      select.add(new Option(planeta, planeta))
+    })
+
+    const output = document.createElement('output')
+
+    select.onchange = () => {
+      output.innerText = select.value
+    }
+
+    this.append(select, output)
+  }
+}
+
+customElements.define('app-root', AppRoot)
